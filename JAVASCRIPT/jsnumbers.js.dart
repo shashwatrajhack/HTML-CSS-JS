c4fb6e6311253ed42b3d5ba9a499435a2bc86083
shashwatrajhack <er.shashwatraj@gmail.com>
2022-10-02 18:31:49 +0530

    js numbers

---
+++ JAVASCRIPT/jsnumbers.js.dart
@@ -0,0 +1,32 @@
+console.clear()
+
+var num1=21;
+var num2=10;
+
+console.log(typeof(num1))
+console.log(typeof(num2))
+
+//addition
+
+var num3=31;
+var add=num1+num3;
+console.log(add);
+
+//multiplication
+
+var mul=num1*num3;
+console.log(mul);
+
+//division
+
+var div=num1/num3;
+console.log(div);
+
+var divByZero=num3/0;
+console.log(divByZero);
+console.log(typeof(divByZero));
+
+var mulByString = num2* 'S';
+console.log(mulByString);
+console.log(typeof(mulByString));
+
