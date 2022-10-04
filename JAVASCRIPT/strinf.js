9986ca04da232cdfb3b4ca481d6cf74fd7e9fc57
shashwatrajhack <er.shashwatraj@gmail.com>
2022-10-05 00:01:13 +0530

    string functions

---
+++ JAVASCRIPT/strinf.js
@@ -0,0 +1,6 @@
+var ss = "  this is javascript tutorial  "
+var rrr = "today is the great date"
+console.log(ss.toUpperCase());
+console.log(ss.toLowerCase());
+console.log(ss.concat(rrr));
+console.log(ss.concat(rrr).trim());
